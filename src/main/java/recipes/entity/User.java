f5eadb8a54154f93d07f6c1401e2b5ef31ff9ab5
pkovacs1991 <p.kovacs1991@gmail.com>
2017-12-19 22:30:31 +0100

    username is now unique

--- src/main/java/recipes/entity/User.java
+++ src/main/java/recipes/entity/User.java
@@ -23,6 +23,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable=false, unique = true)
     private String username;
 
     @Column(nullable=false, unique = true)
